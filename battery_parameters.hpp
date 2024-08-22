1eb84a05d764fb37ccb492af913044d26c5ad239
kousuke1902 <kousuke1902love@gmail.com>
2024-08-22 23:23:55 +0900

    バッテリーパラメータ，パーティクルシステムの情報を追加．
    バッテリーパラメータへ各種パラメータを記載．必要な関数は順次記載．

---
+++ battery_parameters.hpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class BatteryParameters final
+{
+
+private:
+
+	BatteryParameters() = default; // コンストラクタ
+	~BatteryParameters() = default; // デストラクタ
+
+	// 各種パラメータ
+	double charging_power; // 充電量
+	int battery_stock; // 電池ストック
+	int battery_max_stock; // 最大電池ストック
+
+
+public:
+
+	BatteryParameters(const BatteryParameters&) = delete; // コピーコンストラクタを削除指定
+	BatteryParameters& operator = (const BatteryParameters&) = delete; // コピー代入演算子を削除指定
+	BatteryParameters(BatteryParameters&&) = delete; // ムーブコンストラクタを削除指定
+	BatteryParameters& operator = (BatteryParameters&&) = delete; // ムーブ代入演算子を削除指定
+
+	// インスタンスを取得
+	static BatteryParameters& getInstance()
+	{
+		static BatteryParameters inst; //privateのコンストラクタ
+		return inst;
+	}
+
+	// 充電量
+	// 充電量を入力する
+	int setChargingPower(double x)
+	{
+		charging_power = x;
+		return 0;
+	}
+
+};
